770deef65b90ebe9c39e6b76e113eccce30d82d3
Sumafix <gzadkowski@gmail.com>
2020-03-12 19:07:33 +0100

    Added 3rd example for chapter 2
    Program using cin

---
+++ Chapter02/Examples/ex02_03--getinfo.cpp
@@ -0,0 +1,18 @@
+// getinfo.cpp -- wejście i wyjście
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int carrots;
+
+    cout << "Ile masz marchewek?" << endl;
+    cin >> carrots;                             // wczytywanie w C++
+    cout << "Prosze, oto jeszcze dwie. ";
+    carrots = carrots + 2;
+    // w następnym wierszu sklejamy wyniki
+    cout << "Teraz masz " << carrots << " marchewek." << endl;
+    return 0;
+}
