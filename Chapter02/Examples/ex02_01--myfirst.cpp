17319649a402a5b8cdab9c1c5c2f9dc2aaf70554
Sumafix <gzadkowski@gmail.com>
2020-03-12 17:54:36 +0100

    Added 2nd example for chapter 2
    Added comments to ex02_01.cpp
    Created 2nd example for chapter 2 with using variable.

--- Chapter02/Examples/ex02_01--myfirst.cpp
+++ Chapter02/Examples/ex02_01--myfirst.cpp
@@ -1,13 +1,13 @@
 // myfirst.cpp -- wyświetla komunikat
 
-#include <iostream>
+#include <iostream>                         // dyrektywa PREPROCESORA
 
-int main ()
-{
-    using namespace std;
+int main()                                  // nagłówek funkcji
+{                                           // początek treści funkcji
+    using namespace std;                    // uwidocznienie definicji
 
-    cout << "Zabaw sie jezykiem C++.";
-    cout << endl;
-    cout << "Nie pozalujesz" << endl;
-    return 0;
-}
+    cout << "Zabaw sie jezykiem C++.";      // komunikat
+    cout << endl;                           // zaczynamy nowy wiersz
+    cout << "Nie pozalujesz" << endl;       // kolejny komunikat
+    return 0;                               // zakończenie działania funkcji main()
+}                                           // koniec treści funkcji
